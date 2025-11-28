bdfdb34aebb3596bfa0dbe5d4be8f3155f139bab
DevloCore <remi.porquet.pro@gmail.com>
2025-11-28 11:46:36 +0100

    Some fixes

--- src/helper.js
+++ src/helper.js
@@ -2,20 +2,34 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import fs from "fs";
 
-function findProjectRoot(startDir) {
+function findProjectRootFrom(startDir) {
   let currentDir = dirname(dirname(startDir));
 
   // Cherche le fichier 'package.json' dans le répertoire actuel ou ses parents
   while (!fs.existsSync(join(currentDir, 'package.json'))) {
     const parentDir = dirname(currentDir);
-    if (parentDir === currentDir) throw new Error("Project root not found: 'package.json' not found in any parent directory.");
+    if (parentDir === currentDir) throw new Error("Project root not found: 'package.json' not found in any parent directory. " + startDir);
     currentDir = parentDir;
   }
   
   return currentDir;
 }
 
-export const projectRoot = findProjectRoot(dirname(fileURLToPath(import.meta.url)));
+function findProjectRoot() {
+  // 1) Essaye d'abord depuis le working directory (projet qui exécute)
+  try {
+    const cwdReal = fs.realpathSync(process.cwd());
+	return cwdReal;
+  } catch (e) {
+    // 2) Fallback : cas où process.cwd() n'est pas ce qu'on attend (rare)
+    const pkgDir = dirname(fileURLToPath(import.meta.url));
+    // si tu veux commencer un niveau au-dessus comme avant :
+    const start = dirname(pkgDir);
+    return findProjectRootFrom(start);
+  }
+}
+
+export const projectRoot = findProjectRoot();
 
 export function workingDirectory() {
   return process.cwd();
--- src/types/zodMiddleware.d.ts
+++ src/types/zodMiddleware.d.ts
@@ -3,9 +3,9 @@ import { ParamsDictionary } from 'express-serve-static-core';
 import { z, ZodEffects, ZodError, ZodSchema, ZodType, ZodTypeDef, ZodNumber, ZodOptional } from 'zod';
 
 declare module '@devlocore/apipack/zod' {
-    export declare function validateBody<TBody>(zodSchema: ZodSchema<TBody>): RequestHandler<ParamsDictionary, any, TBody, any>;
-    export declare function validateParams<TParams>(zodSchema: ZodSchema<TParams>): RequestHandler<TParams, any, any, any>;
-    export declare function validateQuery<TQuery>(zodSchema: ZodSchema<TQuery>): RequestHandler<ParamsDictionary, any, any, TQuery>;
+    export declare function validateBody<TBody>(zodSchema: ZodSchema<TBody>): RequestHandler<Request['params'], any, TBody, any>;
+    export declare function validateParams<TParams>(zodSchema: ZodSchema<TParams>): RequestHandler<Request['params'] & TParams, any, any, any>;
+    export declare function validateQuery<TQuery>(zodSchema: ZodSchema<TQuery>): RequestHandler<Request['params'], any, any, Request['query'] & TQuery>;
 }
 
 declare module 'express-serve-static-core' {
